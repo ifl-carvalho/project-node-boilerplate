7d081bb2ff4f9a1042631dd1ec27d02023c75184
Ítalo Carvalho <contact.italo.carvalho@gmail.com>
2022-02-27 09:53:43 -0300

    chore: update config & libraries

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,18 @@
 module.exports = {
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  roots: ['<rootDir>/tests'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
-  moduleNameMapper: {
-    '@/tests/(.+)': '<rootDir>/tests/$1',
-    '@/(.+)': '<rootDir>/src/$1'
-  },
-  roots: [
-    '<rootDir>/src',
-    '<rootDir>/tests'
-  ],
+  testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
   transform: {
-    '\\.ts$': 'ts-jest'
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
   }
 }
